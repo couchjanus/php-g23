8ef6dc33fbf411e5f28504a6a433a740396e74cb
couchjanus <couchjanus@gmail.com>
2021-11-19 21:00:50 +0200

    added unit 4

--- app/Controllers/ContactController.php
+++ app/Controllers/ContactController.php
@@ -1,6 +1,28 @@
 <?php
 $address = conf('contacts');
-// var_dump($address);
+
+$con = mysqli_connect('localhost', 'root', '', 'peculiar');
+
+if($_POST){
+$name = mysqli_real_escape_string($con, $_POST['name']);
+$email = mysqli_real_escape_string($con, $_POST['email']);
+$message= mysqli_real_escape_string($con, $_POST['message']);
+$sql = <<<EOT
+    INSERT INTO guestbook (name, email, message)
+    VALUES ('$name', '$email', '$message');
+EOT;
+
+mysqli_query($con, $sql);
+
+}
+
+$sql = "SELECT * FROM guestbook";
+$result = mysqli_query($con, $sql);
+if($result){
+    $items = mysqli_fetch_all($result, MYSQLI_ASSOC);
+}else{
+    echo "Error ".mysqli_error();
+}
 
 // render('/contact/index', ['address' => $address]);
-render('/contact/index', ['address' => $address]);
+render('/contact/index', ['address' => $address, 'messages'=>$items]);
--- app/Views/contact/index.php
+++ app/Views/contact/index.php
@@ -12,3 +12,23 @@
 </ul>
 
 <?php endif;?>
+
+
+<?php if (isset($messages)):?>
+    <?php foreach ($messages as $row):?>
+        <li class="mb-2">
+            Customer <?=$row['name']?> wrote this:
+            <?=$row['message']?> 
+            at: <?=date("d-m-Y", strtotime($row['created_at']))?>
+        </li>
+        <?php endforeach?>
+<?php endif;?>  
+<hr>
+<br>
+<form method="post">
+    <elabel>Name: <input name="name"></elabel><br>
+    <label>Email: <input name="email" type="email"></label><br>
+    <label>Message: <input name="message"></label><br>
+    <input type="submit"><br>
+</form>
+<br>
